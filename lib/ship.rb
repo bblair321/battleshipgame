5ecbf3d591cb598ac74280c322191af36a9fef1d
Brady Blair <bblair321@yahoo.comm>
2025-02-20 14:41:38 -0800

    Added hit and health method with tests

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,6 +1,14 @@
 class Ship
+  attr_reader :name, :length
   def initialize(name,length)
     @name = name
     @length = length
+    @hit = true
   end
+
+  def health
+    @length
+  end
+
+
 end
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -9,5 +9,21 @@ RSpec.describe Ship do
       
     expect(@cruiser).to be_an_instance_of(Ship)
     end
+  end  
+  describe 'has attibutes' do
+    it 'it has name' do
+      
+    expect(@cruiser.name).to eq("Cruiser")
+    end
+    it 'it has length' do
+      
+    expect(@cruiser.length).to eq(3)
+    end
+
+    it 'it has health' do
+      
+    expect(@cruiser.health).to eq(3)
+    end
+
   end
 end

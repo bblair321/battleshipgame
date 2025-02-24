5609210c69e17923d81d3d220c435df14f4aecd4
Ecaterina Profir <ecaterina.profir@gmail.com>
2025-02-23 20:12:06 -0600

    We created another test for the valid coordinates? method to ensure that ship placements folow the required rule by  passing in  examples for each possible scenario expected

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe Board do
     it 'exists and starts with an empty hash' do
       expect(@board).to be_a(Board)
       expect(@board.cells).to be_a(Hash)
-      expect(@board.cells.empty?).to eq(true)  # it`s empty at first
+      expect(@board.cells.empty?).to eq(true)  
     end
   end
 
@@ -20,7 +20,7 @@ RSpec.describe Board do
     it 'can generate cells when requested' do
       @board.generate_cells
 
-      expect(@board.cells.length).to eq(16)  #Now it``s filled with all obj
+      expect(@board.cells.length).to eq(16)  
       expect(@board.cells.keys).to include("A1", "D4")
       expect(@board.cells["A1"]).to be_a(Cell)
       expect(@board.cells["D4"]).to be_a(Cell)
@@ -45,9 +45,33 @@ RSpec.describe Board do
   end
 
   describe "#valid_placement" do
-  it 'returns false if the number of coordinates does not match ship length' do
-    expect(@board.valid_placement?(@cruiser, ["A1", "A2"])).to eq(false)
-    expect(@board.valid_placement?(@submarine, ["B1", "B2", "B3"])).to eq(false)
-  end
+    it 'returns false if the number of coordinates does not match ship length' do
+      expect(@board.valid_placement?(@cruiser, ["A1", "A2"])).to eq(false)
+      expect(@board.valid_placement?(@submarine, ["B1", "B2", "B3"])).to eq(false)
+    end
+    it 'returns true for any placement' do
+      expect(@board.valid_placement?(@submarine, ["A1", "A2"])).to be true
+      expect(@board.valid_placement?(@cruiser, ["B1", "C1", "D1"])).to be true
+    end
+
+    it 'returns true for horizontal placement' do 
+      expect(@board.valid_placement?(@cruiser, ["A1", "A2", "A3"])).to eq(true)
+    end
+
+    it 'returns true for vertical placement' do
+      expect(@board.valid_placement?(@cruiser, ["B1", "C1", "D1"])).to eq(true)
+    end
+
+    it 'returns false for diagonal placement' do
+      expect(@board.valid_placement?(@cruiser, ["A1", "B2", "C3"])).to eq(false)
+    end
+
+    it 'returns false for non-consecutive row placement' do
+      expect(@board.valid_placement?(@cruiser, ["A1", "A3", "A4"])).to eq(false)
+    end
+    
+    it 'returns false for non-consecutive column placement' do
+      expect(@board.valid_placement?(@cruiser, ["A1", "C1"])).to eq(false)
+    end
   end
 end

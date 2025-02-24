38ee97e13af730cd7192d8b08476c879bdb79263
Ecaterina Profir <ecaterina.profir@gmail.com>
2025-02-24 09:28:33 -0600

    We defined the consecutive_coordinates? method

--- lib/board.rb
+++ lib/board.rb
@@ -16,4 +16,39 @@ class Board
       end
     end
   end
-end
+
+  def valid_coordinate?(coordinate)
+    @cells.keys.include?(coordinate)
+  end
+
+  def valid_placement?(ship, coordinates)
+    return false if coordinates.length != ship.length
+    return false unless consecutive_coordinates?(coordinates)
+    true  
+  end
+
+  def consecutive_coordinates?(coordinates)
+    rows = []
+    columns = []
+    coordinates.each do |coord|
+      rows << coord[0]
+      columns << coord[1..-1].to_i
+    end
+    if rows.uniq.length == 1
+      columns.sort!
+      columns.each_cons(2).all? do |first, second|
+       second == first + 1
+      end
+    elsif columns.uniq.length == 1
+      rows.sort!
+      rows.each_cons(2).all? do |first, second|
+        second == first.next
+      end
+    else
+       false
+    end
+     #def 
+  end 
+end 
+
+######
